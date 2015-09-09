1f5f5346286b857135a0cf821c585a00e51e7a10
crccheck <c@crccheck.com>
2015-09-08 23:46:07 -0500

    example 1

---
+++ example/thing.py
@@ -0,0 +1,6 @@
+class Foo():
+    def __init__(self):
+        pass
+
+    def thing(self):
+        raise NotImplementedError()
--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
+import mock
+
+from example.thing import Foo
 
 
 class ExcerciseTests(TestCase):
-    def test_1_thing(self):
-        pass
+    @mock.patch('example.thing.Foo.thing')
+    def test_1_thing(self, mock_thing):
+        obj = Foo()
+
+        #################################
+        mock_thing.return_value = 'xyzzy'
+        #################################
+
+        self.assertEqual(obj.thing(), 'xyzzy')
